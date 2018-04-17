638a77b23105ff86436d15887ddb30c4db5b6163
colinlmacleod1 <colinlmacleod1@gmail.com>
2018-04-16 23:00:13 -0400

    Set up App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <aside className="sidebar">Users</aside>
+        <section className="main">
+          <section className="messages-list">Messages List</section>
+          <section className="new-message">New Message</section>
+        </section>
       </div>
     );
   }
